0b0dd0b785772da4d2eae9ef066b62c00e886076
Ulises <gonzalo.simarro@gmail.com>
2021-06-22 19:02:07 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,17 +2,18 @@
 Created on 2021 by Gonzalo Simarro and Daniel Calvete
 '''
 # own modules
-from ucalib import basis, images
+from ucalib import ucalib
 #
 pathFolderBasis = 'basis'# USER DEFINED (folder with the basis images and their calibrations)
 
-basis.nonlinearCalibrationOfBasis(pathFolderBasis)
+eCrit=5.
+
+ucalib.CalibrationOfBasis(pathFolderBasis,eCrit)
 
 
-fC, KC = 5., 4 # USER DEFINED
 pathFolderImagesToAutoCalibrate = 'imagesToAutoCalibrate' # USER DEFINED (folder with the images to calibrate)
+
 nORB = 10000 # USER DEFINED
+fC, KC = 5., 4 # USER DEFINED
 
-
-images.autoCalibration(pathFolderBasis,pathFolderImagesToAutoCalibrate,fC,KC,nORB)
-
+ucalib.AutoCalibrationOfImages(pathFolderBasis,pathFolderImagesToAutoCalibrate,nORB,fC,KC)
